527dae8deeecf735b7b3f486562c7966819f0151
rostik404 <shtanko243@gmail.com>
2017-05-19 13:21:54 +0300

    fix: fix tests

--- test/unit/runner/index.js
+++ test/unit/runner/index.js
@@ -41,9 +41,7 @@ describe('runner', () => {
     };
 
     const stubBrowserRunner = (scenario) => {
-        BrowserRunner.prototype.run.restore();
-
-        sandbox.stub(BrowserRunner.prototype, 'run').callsFake(function() {
+        BrowserRunner.prototype.run.callsFake(function() {
             return Promise.resolve(scenario(this));
         });
     };
